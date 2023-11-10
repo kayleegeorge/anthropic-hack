2976d41134061fc575b9306c4057642653c143f4
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2023-11-11 00:05:02 +0100

    rm error

--- app/api/lang/route.ts
+++ app/api/lang/route.ts
@@ -1,28 +1,28 @@
-import { StreamingTextResponse, LangChainStream } from 'ai';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { AIMessage, HumanMessage } from 'langchain/schema';
+// import { StreamingTextResponse, LangChainStream } from 'ai';
+// import { ChatOpenAI } from 'langchain/chat_models/openai';
+// import { AIMessage, HumanMessage } from 'langchain/schema';
  
-export const runtime = 'edge';
+// export const runtime = 'edge';
  
-export async function POST(req: Request) {
-  const { messages } = await req.json();
-  const { stream, handlers, writer } = LangChainStream();
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
+//   const { stream, handlers, writer } = LangChainStream();
  
-  const llm = new ChatOpenAI({
-    streaming: true,
-  });
+//   const llm = new ChatOpenAI({
+//     streaming: true,
+//   });
  
-  llm
-    .call(
-      messages.map(m =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content),
-      ),
-      {},
-      [handlers],
-    )
-    .catch(console.error);
+//   llm
+//     .call(
+//       messages.map(m =>
+//         m.role == 'user'
+//           ? new HumanMessage(m.content)
+//           : new AIMessage(m.content),
+//       ),
+//       {},
+//       [handlers],
+//     )
+//     .catch(console.error);
  
-  return new StreamingTextResponse(stream);
-}
+//   return new StreamingTextResponse(stream);
+// }
